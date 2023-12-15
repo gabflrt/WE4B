1615052a74f642eb23d1c7cfd08c820d024e41ff
MatthieuDelisle <44803799+MatthieuDelisle@users.noreply.github.com>
2023-12-15 17:37:32 +0100

    fixed routes

--- WE4B/src/app/app-routing.module.ts
+++ WE4B/src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { RestaurantComponent } from './restaurant/restaurant.component';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 import { Restaurant } from './models/restaurant';
 import { RestaurantFullComponent } from './restaurant-full/restaurant-full.component';
+import { AccountComponent } from './account/account.component';
+import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
   {path:'', component: RestaurantListComponent},
-  {path:'restaurant/:restaurant_id', component: RestaurantFullComponent}  
+  {path:'account', component: AccountComponent},
+  {path:'orders', component: OrdersComponent},
+  {path:'restaurant/:restaurant_id', component: RestaurantFullComponent},
   ];
 
 @NgModule({
