05c60beafdcabdfb1976d1b645d98b1b7d32aede
Gabriel Fleuret <gabfleuret@gmail.com>
2023-12-31 12:53:21 +0100

    recup

--- WE4B/src/app/restaurant.service.ts
+++ WE4B/src/app/restaurant.service.ts
@@ -8,24 +8,24 @@ import { Observable, map } from 'rxjs';
 })
 export class RestaurantService {
 
-  ServArray : Restaurant[] = []
-  restaurant! : Restaurant;
-  constructor(private http : HttpClient) { }
+  ServArray: Restaurant[] = []
+  restaurant!: Restaurant;
+  constructor(private http: HttpClient) { }
 
-  getRestaurants() : Observable<Restaurant[]> {
-    return this.http.get<Restaurant[]>('http://localhost:3000/restaurants')
-    }
-    
-  getData() : Restaurant {
-  
-    return new Restaurant(1,"Auberge-fleurie", "Cuisine Française, Européenne", "2022-01-13", "41 rue des acacias",4.5,"./assets/img/1.jpg", 1);
-  
+  getRestaurants(): Observable<Restaurant[]> {
+    return this.http.get<Restaurant[]>('http://localhost:3000/restaurants/')
+  }
+
+  getData(): Restaurant {
+
+    return new Restaurant(1, "Auberge-fleurie", "Cuisine Française, Européenne", "2022-01-13", "41 rue des acacias", 4.5, "./assets/img/1.jpg", 1);
+
   }
   getPrdByIndex(restaurant_id: number): Restaurant {
-  return this.ServArray[restaurant_id]
+    return this.ServArray[restaurant_id]
   }
 
 
 
 
-  }
+}
